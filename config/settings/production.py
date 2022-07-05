9fcf530f9e48e77d209362c46d9f75b6fe643528
kithera35 <raistlinmajere36@gmail.com>
2022-07-05 22:55:01 +0300

    bucket oluşturuldu

--- config/settings/production.py
+++ config/settings/production.py
@@ -66,7 +66,7 @@ sentry_sdk.init(
 
 AWS_ACCESS_KEY_ID=env("ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=env("SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME="onlinehizmetinizbucket"
+AWS_STORAGE_BUCKET_NAME="onlinehizmetinizbucketvirginia"
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
